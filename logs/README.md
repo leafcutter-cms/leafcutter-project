a78769eccb814d624ef687e67c84ae2d355135f4
Joby Elliott <code@byjoby.com>
2020-05-20 22:52:56 -0600

    reorganized config/logging

---
+++ logs/README.md
@@ -0,0 +1,3 @@
+# Logs directory
+
+By default only errors and higher are logged. You can turn on debug logging in config, by setting `debug_log: true` in a config file. The default of false is set in `config/system.yaml` but setting it in `config/env.yaml` is suggested.
--- web/index.php
+++ web/index.php
@@ -12,12 +12,19 @@ date_default_timezone_set("America/Denver");
 $config = new Config\Config();
 $config['base_dir'] = __DIR__;
 $config->readDir(__DIR__ . '/../config/', null, true);
-$config->readFile(__DIR__ . '/../config/env.yaml', null, true);
+if (is_file(__DIR__ . '/../config/env.yaml')) {
+    $config->readFile(__DIR__ . '/../config/env.yaml', null, true);
+}
 
 //initialize logger
 $logger = new Logger('leafcutter');
+if ($config['debug_log']) {
+    $logger->pushHandler(
+        new StreamHandler(__DIR__ . '/../logs/debug.log', Logger::DEBUG)
+    );
+}
 $logger->pushHandler(
-    new StreamHandler(__DIR__ . '/../debug.log', Logger::DEBUG)
+    new StreamHandler(__DIR__ . '/../logs/error.log', Logger::ERROR)
 );
 
 //initialize URL site and context
